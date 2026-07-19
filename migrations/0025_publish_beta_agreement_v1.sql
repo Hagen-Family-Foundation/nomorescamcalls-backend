fd6f538810ae35486e5c05702fada9c58166bce5
Swede <support@scaminaterbusiness.com>
2026-07-19 17:04:09 -0500

    Publish beta agreement v1

---
+++ migrations/0025_publish_beta_agreement_v1.sql
@@ -0,0 +1,14 @@
+INSERT INTO beta_agreements (
+	version,
+	title,
+	content_hash,
+	effective_at,
+	active
+)
+VALUES (
+	'v1',
+	'NoMoreScamCalls Beta Participant Agreement',
+	'2bfdc5f3f56f6b767d981bb6ed6dd2a14f8852704f0afaf20090b50320162c84',
+	'2026-07-19T00:00:00Z',
+	1
+);
